673ae9d4cdea8fc9e5526edbecfdf7a17764ff9d
Fadi022 <hassan.badigi@gmail.com>
2021-11-25 17:42:08 +0100

    test_app.sh

--- test_app.sh
+++ test_app.sh
@@ -14,6 +14,7 @@ echo "COPY ./test_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 8080" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/test_app.py" >> tempdir/Dockerfile
 
+
  
 #Lancement du build de l'image testapp à partir de Dockerfile sous /tempdir
 cd tempdir
